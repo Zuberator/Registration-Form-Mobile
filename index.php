dfd38077e2b3b0b24809bd038d0b938476a7cec5
Zuberator <90205147+Zuberator@users.noreply.github.com>
2022-05-18 11:52:25 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<html>
+<head>
+<meta charset="utf-8">
+<title>Face.js</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script defer src="js/face-api.min.js"></script>
+<script defer src="js/functions.js"></script>
+<style>
+    body {
+        margin: 0;
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    canvas {
+        position: absolute;
+    }
+</style>
+</head>
+
+<body>
+    <video id="video" width="720" height="560" autoplay muted></video>
+</body>
+
+</html>
